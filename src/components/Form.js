01e1dfe793e7f2a14878e3548ebb88e41182f0af
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2022-12-26 22:01:30 +0300

    add form functionality

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,3 +1,5 @@
+import {useState} from 'react'
+
 import {
     FormControl,
     FormLabel,
@@ -9,25 +11,55 @@ import {
 
 
 export default function Form(){
+
+    const [formData, setFormData] = useState({
+        firstName : '',
+        lastName : '',
+        email : ''
+    })
+
+    function handleChange(e){
+        const {name,value} = e.target
+        setFormData({
+            ...formData,
+            [name] : value
+        })
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log(formData)
+        alert('Thank you for subscribing!')
+        
+        // Reset the form
+        setFormData({
+            firstName: "",
+            lastName: "",
+            email: "",
+          })
+    }
+
     return (
         <section className='px-5 my-10 '>
             <h2 className='text-4xl font-bold text-center py-5 '>Join our Newsletter</h2>
-            <FormControl isRequired style={{ width: '75%' }} className='mx-auto pt-10' >
+            <form onSubmit={handleSubmit} >
+            <FormControl isRequired style={{ width: '75%' }} className='mx-auto pt-10'  >
                 <div className='flex'>
                     <div className='flex-1 mr-5'>
                         <FormLabel>First name</FormLabel>
-                        <Input placeholder='First name' />
+                        <Input placeholder='First name' name='firstName' value={formData.firstName} onChange={handleChange} />
                     </div>
                     <div className='flex-1'>
                         <FormLabel>Last name</FormLabel>
-                        <Input placeholder='Last name' />
+                        <Input placeholder='Last name' name='lastName' value={formData.lastName} onChange={handleChange} />
                     </div>
                 </div>
                 <FormLabel>Email address</FormLabel>
-                <Input type="email" placeholder="Enter your email" />
+                <Input type="email" placeholder="Enter your email" name='email' value={formData.email} onChange={handleChange} />
                 <FormHelperText>We'll never share your email.</FormHelperText>
                 <Button type='submit' colorScheme='' className='bg-red-400' mt={5}>Submit</Button>
             </FormControl>
+            </form>
 
         </section>
 
