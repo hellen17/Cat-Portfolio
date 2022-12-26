5ea1953e9134ab5b02627d1df677fea946e0fc38
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2022-12-26 10:33:49 +0300

    add logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,25 +2,38 @@ export function Navbar() {
     const styles = {
         nav: {
             display: 'flex',
-            justifyContent: 'right',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
             alignItems: 'center',
             padding: '0 2rem',
             backgroundColor: 'black',
             color: 'white',
-            height: '4rem'
+            height: '4rem',
+            
         },
         a: {
             textDecoration: 'none',
             color: 'white',
             margin: '0 1rem',
+        },
+
+        logoContainer: {
+            display: 'flex',
+            alignItems: 'center'
         }
     }
 
     return (
         <nav style={styles.nav}>
-            <a style={styles.a} href="/">Home</a>
-            <a style={styles.a} href="">Images</a>
-            <a style={styles.a} href="">Sign Up</a>
+            <div style={styles.logoContainer}> 
+                <img src="/path/to/logo.png" style={{width: '3rem', height: '3rem'}} /> 
+            </div>
+            <div style={{ display: 'flex' }}>
+                <a style={styles.a} href="/">Home</a>
+                <a style={styles.a} href="">Images</a>
+                <a style={styles.a} href="">Sign Up</a>
+            </div>
+        
         </nav>
     )
 }
