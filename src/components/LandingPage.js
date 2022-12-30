0030726060c44322dc88520772e80ed3b49f5f55
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2022-12-30 08:17:18 +0300

    edit padding of heroshot

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -16,7 +16,7 @@ export default function LandingPage() {
                     <div className="md:w-full mx-auto">
                         <img className='rounded text-center' src='images/heroshot.png' alt='Cat' />
                     </div>
-                    <div className="text-left" style={{ padding: '8rem' }}>
+                    <div className="text-left lg:p-32 p-8">
                         <h1 className="text-4xl font-bold pb-5">Designed for Cat Lovers</h1>
                         <p className="text-2xl">Our furbabies come in all shape colors and sizes and deserve to be admired.</p>
                         <Button colorScheme='' className='mt-5 bg-red-400 hover:bg-red-500' >Get Started</Button>
