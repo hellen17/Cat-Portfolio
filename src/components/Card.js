9b051e8df4d8af1a046637c457145e3ce326b35d
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2023-01-24 15:29:08 +0300

    create profile page displaying liked images

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,16 +4,25 @@ import { FaHeart, FaRegHeart } from 'react-icons/fa';
 
 
 export default function CatCards({name, age, color, url, breed}){
-
     const [liked, setLiked] = useState(false)
 
+    let likedCats = JSON.parse(localStorage.getItem('likedCats')) || []
 
     const handleClick = () => {
         setLiked(!liked)
+        //add liked image to local storage
+        if (liked){
+            likedCats = likedCats.filter(cat => cat.name !== name)
+        }
+        else{
+            likedCats.push({name, age, color, url, breed, liked:true})
+        }
+        localStorage.setItem('likedCats', JSON.stringify(likedCats))
     }
+    
 
     return (
-
+        
         <Card className='shadow-lg'>
             <CardHeader>
                 <Image
@@ -27,12 +36,17 @@ export default function CatCards({name, age, color, url, breed}){
                 <Text>Age: {age}</Text>
                 <Text>Color: {color}</Text>
                 <Text>Breed: {breed}</Text>
-                <span onClick={handleClick}>{liked ? <FaHeart /> : <FaRegHeart />}</span>
+                {/* <span onClick={handleClick}>{liked ? <FaHeart /> : <FaRegHeart />}</span> */}
+                <span onClick={handleClick}>
+                {likedCats.find(cat => cat.name === name && cat.age === age && cat.color === color && cat.url === url && cat.breed === breed)?.liked ? <FaHeart color='red' size={20} /> : <FaRegHeart size={20} />}
+                </span>
                
 
                 {/* <Button onClick={handleClick}>Like</Button>  */}
             </CardBody>
         </Card>
+
+       
     )
 }
 
