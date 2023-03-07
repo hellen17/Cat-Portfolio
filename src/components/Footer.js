e6c10cc6aa57b613317e6c9b45d150c8fb367f9a
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2023-03-07 23:41:20 +0300

    edit footer links and add copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
-import { Link } from '@chakra-ui/react'
+import { Link } from 'react-router-dom';
 import React from 'react'
 
 export default function Footer(){
 
+    const year = new Date().getFullYear();
 
     return(
         <footer className="bg-gray-800 py-10 text-gray-400 text-xl lg:px-5 text-center lg:text-left" >
@@ -11,17 +12,17 @@ export default function Footer(){
                     <img className="mx-auto sm:mx-auto lg:mx-0" src="images/logo.png" style={{width: '3rem', height: '3rem' }} /> 
 
                     <h3 className='text-2xl font-bold my-5'>About Us</h3>
-                    <p className='text-gray-600 mb-8'>A website designed for Cat Lovers. </p>                
+                    <p className='text-gray-500 mb-8'>A website designed for Cat Lovers. </p>                
                 </div>
                 <div className="w-full lg:w-1/3 flex-1">
                     <div>
-                        <Link href="#">Home</Link>
+                        <Link to="/">Home</Link>
                     </div>
                     <div>
-                        <Link href="#">About us</Link>
+                        <Link to="#">Images</Link>
                     </div>
                     <div>
-                        <Link href="#">Contact us</Link>
+                        <Link to="/vets">Browse Vets</Link>
                     </div>
 
                 </div>
@@ -30,25 +31,12 @@ export default function Footer(){
                 </div>
             </div>
 
+            {/* add copyright */}
+            <div className="mt-10 text-center">
+                <p className="text-gray-400">© {year} <a href='https://hellenkokach.me/'>Hellen Kokach</a>. All rights reserved.</p>
+            </div>
+
         </footer>
-
-            // <footer className='bg-black py-10'>
-            //     <div className='container mx-auto px-5'>
-            //         <div className='flex flex-wrap'>
-            //             <div className='w-full md:w-1/2 lg:w-1/3'>
-            //                 <h3 className='text-2xl font-bold mb-5'>About Us</h3>
-            //                 <p className='text-gray-600 mb-8'>A website designed for Cat Lovers. </p>
-            //                 <div className='flex items-center'>
-            //                     <svg className='w-6 h-6 fill-current text-gray-600 mr-4' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 0"/><path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 0"/><path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 0"/><path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 0"/><path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 0"/><path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 0"/><path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 0"/><path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8"
-            //                     ></path></svg>
-            //                     <p className='text-gray-600'>+1 234 567 890</p>
-            //                 </div>
-                       
-            //                 </div>
-            //                 </div>
-            //     </div>    
-            // </footer>
     )
-
 
 }
--- src/components/Services.js
+++ src/components/Services.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function Services() {
     return (
         <section className='px-5 my-10 pb-10'>
-        <h2 className='text-4xl font-bold text-center py-5'>What We Do</h2>
+        <h2 className='text-4xl font-bold text-center py-5'>Explore Our Services</h2>
         <div className='grid grid-cols-1 lg:grid-cols-2 gap-10 mt-10 text-center'>
             <div className='rounded-lg p-5 lg:w-3/4 bg-white border border-pink-300'>
                 <div className="flex justify-center">
@@ -23,7 +23,7 @@ export default function Services() {
                 <p className='text-xl mt-3'>
                     View our gallery of cat images. You can also upload your own cat images
                 </p>
-                <Button colorScheme='' className='mt-5 bg-red-400 hover:bg-red-500' ><Link to='/sitters'>Browse</Link></Button>
+                <Button colorScheme='' className='mt-5 bg-red-400 hover:bg-red-500' ><Link to='#'>Browse</Link></Button>
             </div>
             
         </div>
