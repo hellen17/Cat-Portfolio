087eab19e6288184c523f6687921cda1ff7ef942
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2023-03-08 21:50:51 +0300

    add pagination

--- server/data/vetData.js
+++ server/data/vetData.js
@@ -37,16 +37,69 @@ const vetData = [
     },
     {
         "id": 5,
-        "name": ["Dr. John Mwangi"],
-        "city": "Mombasa",
-        "location": "Ambulatory (mobile)",
-        "clinicName": "",
-        "phone": ["+254 722 815882"],
+        "name": ["Dr. Allan Elavula"],
+        "city": "Nairobi",
+        "location": "Ruaka",
+        "clinicName": "Ela veterinary clinic",
+        "phone": ["+254 790 878333", "+254 723 790766"],
+        "email": "elavetclinics@gmail.com"
+    },
+    {
+        "id": 6,
+        "name": ["Dr. Ndurumo M. Stephen"],
+        "city": "Nairobi",
+        "location": "Kongoni Road, Hardy, Karen",
+        "clinicName": "Hardy Veterinary Clinic",
+        "phone": ["0728962833", "0768015191"],
+        "email": "dr.nsteve@gmail.com"
+    },
+    {
+        "id": 7,
+        "name": ["Dr. Andrew Gitari"],
+        "city": "Nairobi",
+        "location": "Ngong’ Road",
+        "clinicName": "The Andys Veterinary Hospital",
+        "phone": ["0722752182", "0708622831", "0722245352"],
+        "email": "andysvetclinic@gmail.com"
+
+    },
+    {
+        "id": 8,
+        "name": ["Dr. Cohen Onsoti"],
+        "city": "Nairobi",
+        "location": "Ambulatory & house calls",
+        "clinicName": "Cohen's Petcare",
+        "phone": ["+254 713139852"],
+        "email": "cohenspetcare@gmail.com"
+    },
+    {
+        "id": 9,
+        "name": ["Dr. Griffins Mwendwa Mulonzi"],
+        "city": "Nairobi",
+        "location": "Exit 10, Kahawa Sukari, Off Thika Road",
+        "clinicName": "The Noble Veterinary Surgeons",
+        "phone": ["+254722657719"],
+        "email": "info@thenoblevet.com"
+    },
+    {
+        "id": 10,
+        "name": ["Dr. Omosa Ochwang’"],
+        "city": "Nairobi",
+        "location": "James Gichuru Road, Lavington",
+        "clinicName": "St. Austin’s Veterinary Clinic, & University of Nairobi",
+        "phone": [""],
+        "email": "0723-759997"
+
+    },
+    {
+        "id": 11,
+        "name": ["Prof. Susan Mbugua"],
+        "city": "Nairobi",
+        "location": "James Gichuru Road, Lavington",
+        "clinicName": "St. Austin’s Veterinary Clinic",
+        "phone": ["0722-746020"],
         "email": ""
-    }
-
-
-
+    },
 
 ]
 
