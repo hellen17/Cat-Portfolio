f763f6bb41582964faefc99c46948d984dfc76c0
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2023-03-08 20:07:02 +0300

    add server folder

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const vetData = require('./data/vetData');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.json(vetData);
+    }
+);
+
+app.listen(5000, () => {
+    console.log('Example app listening on port 5000!');
+    }
+);
