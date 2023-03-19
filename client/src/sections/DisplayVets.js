a6bee622e78eaef4652e484e61d13ca403889199
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2023-03-19 13:55:34 +0300

    created netlfiy function to fetch vet Data

--- client/src/sections/DisplayVets.js
+++ client/src/sections/DisplayVets.js
@@ -20,7 +20,7 @@ export default function DisplayVets() {
 
     useEffect(() => {
         const fetchVetData = async () => {
-            const response = await fetch("http://localhost:5000")
+            const response = await fetch("http://localhost:9999/.netlify/functions/fetch-data")
             const data = await response.json()
             setData(data)
         }
